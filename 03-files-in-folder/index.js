99429d5a4da30c79f84dcbdb36f74e603cbbc90b
Golomuzdov <anton210027@gmail.com>
2022-05-23 21:41:28 +0300

    feat: add solution 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
+  for (let file of files) {
+    fs.stat(path.join(__dirname, 'secret-folder', file), (err, stats) => {
+      if (!stats.isDirectory()) {
+        const baseName = path.basename(file, path.extname(file));
+        const extName = path.extname(file).slice(1);
+        const size = stats.size;
+        console.log( `${baseName} - ${extName} - ${size}kb`);
+      }
+    });
+  }
+});
