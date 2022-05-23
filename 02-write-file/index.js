e1eeaecc5f6d11b7f2659a7a1458136cbb379a90
Golomuzdov <anton210027@gmail.com>
2022-05-23 21:30:47 +0300

    fix: spelling code

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 const {stdin, stdout} = process;
-
 const text = path.join(__dirname, 'text.txt');
 const stream = fs.createWriteStream(text);
 
 stdout.write('Пожалуйста, введите текст');
-
 stdin.on('data', data => {
     if(data.toString().trim() === 'exit') {
         process.exit();
@@ -16,5 +14,4 @@ stdin.on('data', data => {
 });
 
 process.on('exit', () => stdout.write('Прощайте'));
-
 process.on('SIGINT', () => process.exit())
